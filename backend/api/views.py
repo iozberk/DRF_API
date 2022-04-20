063496c51398ae4b4085ca18e08e03a9ea1a8d32
iozberk <iozberks@gmail.com>
2022-04-20 17:22:29 +0300

    01

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import re
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+def api_home(request, *args, **kwargs):
+    print(request.GET)
+    print(request.POST)
+    body = request.body # byte string of Json data
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass    
+    print(data)
+    # data['headers'] = request.headers
+    print(request.headers)
+    data['params'] = dict(request.GET) 
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- pyclient/basic.py
+++ pyclient/basic.py
@@ -2,10 +2,10 @@ import requests
 
 endpoint = 'https://www.httpbin.org/status/200/'
 endpoint = 'https://www.httpbin.org/anything'
-endpoint = 'http://localhost:8000/'
-get_response = requests.get(endpoint) # HTTP Request
+endpoint = 'http://localhost:8000/api'
+get_response = requests.get(endpoint, json={'product_id': 123}) # HTTP Request
 
-print(get_response.text) # raw text response
+# print(get_response.text) # raw text response
 
 print(get_response.json())
 print(get_response.status_code)
