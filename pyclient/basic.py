8cf8a791d3b395b85597d5ba8dfbc7d7dcda7c43
iozberk <iozberks@gmail.com>
2022-04-20 15:45:31 +0300

    Api app created

--- pyclient/basic.py
+++ pyclient/basic.py
@@ -1,4 +1,17 @@
 import requests
 
+endpoint = 'https://www.httpbin.org/status/200/'
+endpoint = 'https://www.httpbin.org/anything'
+endpoint = 'http://localhost:8000/'
+get_response = requests.get(endpoint) # HTTP Request
 
-endpoint = ''
+print(get_response.text) # raw text response
+
+print(get_response.json())
+print(get_response.status_code)
+
+
+
+
+
+
