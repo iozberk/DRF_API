ab0f8f793e8e2611cc2eecdb5df866373d9537fd
iozberk <iozberks@gmail.com>
2022-04-20 23:31:45 +0300

    03

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,22 +3,21 @@ from products.models import Product
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
 
 
 
-
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
     """
     DRF ApiView
     """
-    if request.method != 'POST':
-        return Response({'detail': 'GET not allowed'}, status=400)
-    model_data = Product.objects.all().order_by('?').first()
+    instance = Product.objects.all().order_by('?').first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id','title','content','price'])
+    if instance:
+        # data = model_to_dict(instance, fields=['id','title','content','price','sale_price'])
+        data = ProductSerializer(instance).data
     return Response(data)
 
 
